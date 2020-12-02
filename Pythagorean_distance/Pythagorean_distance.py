d3b6e355c2e30500641a6bea92623107394f6d39
juliancastaeda <1277@holbertonschool.com>
2020-12-01 23:50:34 -0500

    add file with calculate pythagorean distance

---
+++ Pythagorean_distance/Pythagorean_distance.py
@@ -0,0 +1,10 @@
+import math
+'''
+This program calculates the Pythagorean distance between 3 points x, y and z.
+'''
+x, y, z = 2, 3, 4
+
+hipotenusa_1 = (x * x) + (y * y) + (z * z)
+hipotenusa_1 = math.sqrt(hipotenusa_1)
+
+print(hipotenusa_1)
