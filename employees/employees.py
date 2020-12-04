e3873b2f17e892835f9f25908a5a794e9283e567
juliancastanedaDev <juliandavidcastaneda@gmail.com>
2020-12-03 22:45:10 -0500

    Load file employees.py

---
+++ employees/employees.py
@@ -0,0 +1,6 @@
+employees = [['John Mckee', 38, 'Sales'], ['Lisa Crawford', 29, 'Marketing'], ['Sujan Patel', 33, 'HR']]
+for employee in employees:
+    print('name: {}'.format(employee[0]))
+    print('age: {}'.format(employee[1]))
+    print('Department: {}'.format(employee[2]))
+    print('-' * 30)
